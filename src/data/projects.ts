c16451f542e1913da38e481fbd2d3975f8c2d3cb
Helen <4635815+herenhuang@users.noreply.github.com>
2024-12-14 13:30:29 -0500

    Update projects.ts

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,103 +1,97 @@
-export const work = [
+// src/data/projects.ts
+
+export const personalWork = [
   {
-    title: "strategic generalist @ freelance",
-    description: "reach out if you'd like help with product, ops, or marketing!"
+    title: "Freelance Strategist",
+    description: "Helping clients with product, ops, and marketing."
   },
   {
-    title: "cofounder @ co.lab",
-    description: "experiential tech training programs for adult learners",
+    title: "Co.Lab Founder",
+    description: "Scaling experiential tech training programs for adult learners."
+  }
+];
+
+export const professionalWork = [
+  {
+    title: "Strategic Generalist @ Freelance",
+    description: "Reach out if you'd like help with product, ops, or marketing!"
+  },
+  {
+    title: "CoFounder @ Co.Lab",
+    description: "Experiential tech training programs for adult learners",
     link: "https://joincolab.io/"
   },
   {
-    title: "cofounder @ solana portraits",
-    description: "1:1 custom nfts",
+    title: "Program + Product @ Microsoft",
+    description: "Microsoft Edge DevRel, Azure DevOps",
+    link: "https://devblogs.microsoft.com/devops/azure-artifacts-billing-changes-coming-october-2020/"
+  }
+];
+
+export const personalProjects = [
+  {
+    title: "Solana Portraits",
+    description: "Custom NFTs made just for you.",
     link: "https://www.solanaportraits.com/"
   },
   {
-    title: "program + product @ microsoft",
-    description: "microsoft edge devrel, azure devops",
-    link: "https://devblogs.microsoft.com/devops/azure-artifacts-billing-changes-coming-october-2020/"
-  },
-  {
-    title: "product @ zynga",
-    description: "a certain social word game on mobile, 250K DAU"
+    title: "You Belong in Tech",
+    description: "Career transition guides and personal stories of hope.",
+    link: "https://www.producthunt.com/products/you-belong-in-tech-1-0"
   }
 ];
 
-export const projects = [
+export const professionalProjects = [
   {
     title: "Co.Lab Learning",
-    description: "peer-led hybrid live learning platform, over 30k+ learning hours",
+    description: "Peer-led hybrid live learning platform with over 30K learning hours.",
     link: "https://www.producthunt.com/products/co-lab-learning"
   },
   {
-    title: "how to product",
-    description: "ebook, #1 product of the day on Product Hunt",
+    title: "How to Product",
+    description: "Ebook, #1 product of the day on Product Hunt",
     link: "https://www.producthunt.com/products/how-to-product"
-  },
-  {
-    title: "you belong in tech",
-    description: "career transition guides and stories of hope, 20K+ downloads",
-    link: "https://www.producthunt.com/products/you-belong-in-tech-1-0"
-  },
-    {
-    title: "covibes",
-    description: "infinite scrolling site with hopeful covid news",
-    link: "https://hellohelen.substack.com/p/day-62-my-disappearance-"
   }
 ];
 
-export const awards = [
+export const personalAwards = [
   {
-    title: "Forbes 30 under 30",
-    description: "made the north america list with my Co.Lab cofounder",
+    title: "Forbes 30 Under 30",
+    description: "Made the North America list with my Co.Lab cofounder",
     link: "https://www.forbes.com/profile/colab/"
-  },
+  }
+];
+
+export const professionalAwards = [
   {
-    title: "DMZ women of the year",
-    description: "impact in tech and education",
+    title: "DMZ Women of the Year",
+    description: "Impact in tech and education",
     link: "https://dmz.torontomu.ca/dmzs-women-of-the-year-2024/"
   },
   {
-    title: "Glory 30x30 honoree",
-    description: "young canadian entrepreneurs list",
+    title: "Glory 30x30 Honoree",
+    description: "Young Canadian entrepreneurs list",
     link: "https://www.glory.media/glory-professional-30x30-2023/"
   }
 ];
 
-export const talks = [
+export const personalTalks = [
+  {
+    title: "Founder Institute",
+    description: "Community-led and product-led growth",
+    link: "https://www.youtube.com/watch?v=4MMUpWPkNcc"
+  }
+];
+
+export const professionalTalks = [
   {
     title: "Waterloo Innovation Summit",
-    description: "flipped classrooms and social peer-led learning",
+    description: "Flipped classrooms and social peer-led learning",
     link: "https://uwaterloo.ca/waterloo-innovation-summit/"
   },
   {
     title: "DMZ",
-    description: "founder-led sales and full-funnel marketing",
+    description: "Founder-led sales and full-funnel marketing",
     link: "https://www.eventbrite.ca/e/personal-branding-and-founder-led-sales-tickets-723934877707"
-  },
-  {
-    title: "Founder Institute",
-    description: "community-led and product-led growth",
-    link: "https://www.youtube.com/watch?v=4MMUpWPkNcc"
-  },
-  {
-    title: "University of Waterloo global futures",
-    description: "training tech talent of the future",
-    link: "https://uwaterloo.ca/news/global-futures/training-tech-talent-future"
-  },
-  {
-    title: "Elevate conference",
-    description: "pivoting careers as a non-technical technologist"
-  },
-  {
-    title: "SiriusXM",
-    description: "future of edtech and hybrid work in Canada",
-    link: "https://x.com/CanadaTalks167/status/1612471780261732353"
-  },
-  {
-    title: "Github education",
-    description: "product management internships and how to get them",
-    link: "https://github.blog/developer-skills/career-growth/lets-talk-about-tech-internships/"
   }
 ];
