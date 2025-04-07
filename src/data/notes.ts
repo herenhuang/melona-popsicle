341f187160455e4ab7db14682ad7df52984a93a8
Helen Huang <4635815+herenhuang@users.noreply.github.com>
2025-04-07 00:52:39 -0400

    Update notes content and adjust spacing between bullets

--- src/data/notes.ts
+++ src/data/notes.ts
@@ -55,21 +55,22 @@ export const notes: Note[] = [
     content: `howdy, I'm Helen Huang, a product, ops and strategy person. enthusiastic about anything experiential: marketing, content, learning, community, food. 
     
 # currently
-- on a x-month sabbatical (since Sept 2024)
+- having fun on a [career break](https://www.linkedin.com/posts/heyohelen_newbeginnings-careerchange-adultgapyear-activity-7269806988396490752-Lbet?utm_source=share&utm_medium=member_desktop&rcm=ACoAAA0TBRAB1SrfwkAumixBnBxq7Zxt35jGKd8) (since Sept 2024)
 - creating short-form [content](/projects) (31K+ followers, 1M+ impressions on socials)
 - searching for meaningful work with people who care about making the world better (NYC or remote)
 
 # previous work work 
-- cofounder @ co.lab - wearer of all hats, $30M+ in annual tech industry salaries for our graduates, forbes 30 under 30 education, asu+gsv 150, hustle fund affies award
-- program + product management @ microsoft — msft edge tech evangelism & later windows engineering/azure devops working on pricing and developer experience
-- product manager intern @ zynga — wordstreak with friends (300k dau), spearheaded app revamp from 1 to 4 stars in four months
-- interned @ cibc & scotiabank — business analyst roles back in university days
-- sales - manchuwok, canada's wonderland, shoe club  
+- cofounder @ co.lab -- wearer of all hats, $30M+ in annual tech industry salaries for our graduates
+  - forbes 30 under 30 education, asu+gsv 150 top startups, hustle fund affies award, see other [accolades](/recognition)
+- program + product management @ microsoft -- msft edge tech evangelism & later windows engineering/azure devops working on pricing and developer experience
+- product manager intern @ zynga -- wordstreak with friends (300k dau), spearheaded app revamp from 1 to 4 stars in four months
+- interned @ cibc & scotiabank -- business analyst roles back in university days
+- sales -- manchuwok, canada's wonderland, shoe club  
 
 # education
 - b.sc in science and business (earth sci specialization) @ university of waterloo (2017)
-- nat eliason's build your own products with AI course (2025)
-- shreyas doshi's improving your product sense course (2025)`,
+- nat eliason's build your own products with AI course
+- shreyas doshi's improving your product sense course`,
     isPinned: true,
     pinnedOrder: 1
   },
@@ -89,13 +90,28 @@ export const notes: Note[] = [
     id: 'projects',
     title: '📙 projects',
     date: '2025-03-17T00:50:00Z',
-    content: `i love building things (even if they're not perfect). ill upload pics soon. :) 
+    content: `i really like doing stuff, here are some of them i'm particularly proud of: 
 
-- [baggy: a trash bag fashion show](https://helenhuang.io/baggy)
-- co.lab learning: a group-driven, blended learning environment with 30k+ hours of collaboration
-- how to product: e-book, #1 product of the day, sharing product management insights
-- solana portraits: nft art commissions for digital collectible enthusiasts
-- you belong in tech: career-switch stories and resources to inspire folks entering tech`,
+  # events & community
+  - conceptualized and executed satirical performance art project selling 🗑️ trash bags as clothes and running a fashion show
+    - check out the dedicated [BAGGY site](https://helenhuang.io/baggy) to decide whether it's real or fake...
+    - how it all [started](https://www.instagram.com/p/DFBvhI9vayw/) 
+  - coordinated over 15+ volunteers to put on Tech Together, a month-long series of event programming for folks impacted by layoffs with virtual and irl events in Toronto, NYC and Seattle
+
+  ![a few of the events we ran, which saw over 700 engaged attendees (tears were shed)](/images/techtogether.png)
+
+  # content & social media 
+  - micro-influencer on [LinkedIn](https://www.linkedin.com/in/heyohelen/), mainly written content on tech and entrepreneurship (20K+ followers, previously Top Voice)
+  - university of waterloo science faculty [collab reel](https://www.instagram.com/reel/CqlMjmQATLX/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==) -- *35K+ views*
+  - balam pichkari [dance video](https://x.com/heyohelen/status/1651703013361238016) in india -- *250K+ impressions total*
+  - travels to nigeria: self-choreo [dance video](https://x.com/heyohelen/status/1706392849934135705), and written travel [impressions](https://www.linkedin.com/posts/heyohelen_ive-been-in-lagos-for-a-few-days-now-activity-7079138712269295616-QIXu?utm_source=share&utm_medium=member_desktop&rcm=ACoAAA0TBRAB1SrfwkAumixBnBxq7Zxt35jGKd8) -- *900K+ impressions*
+
+  # storytelling 
+  - sold and minted 60+ 1:1 nfts on solana, commission-based community & art project [(example)](https://x.com/tamarincrypto/status/1486887134992941058)
+  - tales on how to pivot into [product management](https://www.producthunt.com/products/how-to-product#how-to-product), #1 product of the day of Product Hunt
+  - [you belong in tech](https://www.producthunt.com/products/you-belong-in-tech-1-0#you-belong-in-tech-1-0) ebooks, #4 product of the day, career-pivot stories and resources meant to inspire folks breaking into tech
+  
+  `,
     isPinned: true,
     pinnedOrder: 4
   },
